8380847a75ff7486887b248052d2b2711130733a
LITTAVANH PHANALASY <littavanhphanalasy@gmail.com>
2024-04-26 10:46:17 +0700

    Update c_topup.php

--- wallet/c_topup.php
+++ wallet/c_topup.php
@@ -3,27 +3,23 @@ $tokenKey = $_SESSION['im_tokenKey'];
 $userName = $_SESSION['im_userName'];
 if (isset($_POST["btnTopUp"])) {
 
-    if (isset($_POST['type'])) {
-        $type = count($_POST['type']);
+    if (isset($_POST['cbCheckOne'])) {
+        $type = count($_POST['cbCheckOne']);
         // echo "<script>alert('type: $type');</script>";
 
         for ($i = 0; $i < $type; $i++) {
 
             $cbCheckOne = $_POST['cbCheckOne'][$i];
-            // $txtStaffID = escape_string($_POST['txtStaffID'][$i]);
-            // $amount = $_POST['txtAmount'];
-            
-            $amount = str_replace(",","",escape_string(trim($_POST['txtAmount'])));
 
-            
-        //  $year = substr($_POST['txtMonth'],0,4);
-        //  $month = substr($_POST['txtMonth'],5,7);
-        $year = date('Y', strtotime($_POST['txtMonth'])); 
-        $month =  date('m', strtotime($_POST['txtMonth'])); 
-         
+       
+            $amount = str_replace(",", "", escape_string(trim($_POST['txtAmount'])));
+
+            $year = date('Y', strtotime($_POST['txtMonth']));
+            $month = date('m', strtotime($_POST['txtMonth']));
+
             if ($cbCheckOne != '') {
-         
-                topupStaffAmount($tokenKey, $userName, $amount, $cbCheckOne,$month,$year, $api_url);
+             
+                topupStaffAmount($tokenKey, $userName, $amount, $cbCheckOne, $month, $year, $api_url);
             }
         }
     }
@@ -31,20 +27,3 @@ if (isset($_POST["btnTopUp"])) {
 
 
 
-// if (isset($_POST["btnRefund"])) {
-//     if (isset($_POST['type'])) {
-//         $type = count($_POST['type']);
-//         // echo "<script>alert('type: $type');</script>";
-
-//         for ($i = 0; $i < $type; $i++) {
-
-//             $cbCheckOne = $_POST['cbCheckOne'][$i];
-//             // $txtStaffID = escape_string($_POST['txtStaffID'][$i]);
-//             $amount = $_POST['txtAmount'];
-//             if ($cbCheckOne != '') {
-//                 // echo "<script>alert('cbCheckOne: $cbCheckOne');</script>";
-//                 refundStaffAmount($tokenKey, $userName, $amount, $cbCheckOne, $api_url);
-//             }
-//         }
-//     }
-// }
